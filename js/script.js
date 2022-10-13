67bf7dcd42e6b041fd3081a3c49ec2818c408d64
aagaev <agaeana@gmail.com>
2022-10-13 21:59:34 +0400

    feat: add conditions

--- js/script.js
+++ js/script.js
@@ -6,16 +6,26 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''), 
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');  
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB)
-//console.log(a)
 
-
-
-
-
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    if (a != null && b!= null && a!= '' && b!= '' && a.length <50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--
+    }      
+}   
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов')
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+}  else {
+    console.log("Произошла ошибка");
+}
+ 
+console.log(personalMovieDB);
